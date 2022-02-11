c9822411ba6e211aea01e18586b1dbb412362477
mael1657 <jhmael013@gmail.com>
2022-02-11 14:02:58 +0900

    first commit

---
+++ src/components/DictCard.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const DictCard = () => {
+    return(
+        <div className="dict-card">
+            <p>hello</p>
+        </div>
+    )
+}
+
+export default DictCard;
